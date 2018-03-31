a9702fa154b210c536da981276f3529a8bd4a3f3
Ally Netzel <netzelally@gmail.com>
2018-03-31 14:25:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
   array.each_with_index { |name, index|
     hash[name] = index
   }
-  puts hash
+  puts #{index+1} #{name}
 end
 
 def summon_captain_planet# code an argument here
