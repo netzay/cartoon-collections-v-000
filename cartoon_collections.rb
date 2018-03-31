625e6d9303eff7cf5a8e001c8524df196dfd1f44
Ally Netzel <netzelally@gmail.com>
2018-03-31 16:07:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,7 @@ def long_planeteer_calls(calls)
     return true
   end
 end
-a = [ "a", "b", "c" ]
-a.include?("b")   #=> true
+
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.include?(array)
