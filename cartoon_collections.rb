93995a01ece6c9d25c7e0fc128f4759f9b4a5da9
Ally Netzel <netzelally@gmail.com>
2018-03-31 14:47:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ end
 
 def summon_captain_planet(array)
   array.map! {|item| item.capitalize }
-  puts 
-
+  array.each {|item| + "!"}
+  return array
 end
 
 def long_planeteer_calls# code an argument here
