a34d7ec2bece8da5c3c3a5ab48ee602e65209814
Ally Netzel <netzelally@gmail.com>
2018-03-31 14:46:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map! {|item| item.capitalize } # now names contains ['Danil', 'Edmund']
+  array.map! {|item| item.capitalize }
+  puts 
 
 end
 
