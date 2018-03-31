3f6a3b9f3a09f44adcb59175e5b6b8e4434fb326
Ally Netzel <netzelally@gmail.com>
2018-03-31 14:44:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
