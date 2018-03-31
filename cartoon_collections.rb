f21da1493f2823ed8dc4af121ebd4a7f7b187e2a
Ally Netzel <netzelally@gmail.com>
2018-03-31 14:51:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map! {|item| item.capitalize }
+  array.map { |item, i| x * i }
   array.collect {|item| item + "!"}
   return array
 end
