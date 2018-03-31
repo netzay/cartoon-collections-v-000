60cbb7ac07bb563bccdd224348dd5ec2c4c8355f
Ally Netzel <netzelally@gmail.com>
2018-03-31 15:23:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  if array.include?(cheese_types)
+  if array.include?(:cheese_types)
     return array.each 
   else
   return nil
